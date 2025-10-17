b712bde74a253426f86bf9727d150545e9a95e45
Sajinits2018 <udemysajinits24@gmail.com>
2025-10-17 11:10:38 +0530

    Update agility-page.ts

--- lib/agility-page.ts
+++ lib/agility-page.ts
@@ -1,24 +1,23 @@
 // lib/agility-page.ts
 import dynamic from "next/dynamic";
 
-// Dynamically import your Workup components
-const WorkupHeader = dynamic(() => import("../components/agility-components/WorkupHeader"));
-const WorkupHero = dynamic(() => import("../components/agility-components/WorkupHero"));
+// Import only components that exist
+const WorkupHeroSlider = dynamic(() => import("../components/agility-components/WorkupHeroSlider"));
+const WorkupFeatures   = dynamic(() => import("../components/agility-components/WorkupFeatures"));
 const WorkupServicesGrid = dynamic(() => import("../components/agility-components/WorkupServicesGrid"));
-// const WorkupTestimonials = dynamic(() => import("../components/agility-components/WorkupTestimonials"));
-// const WorkupWhyChoose = dynamic(() => import("../components/agility-components/WorkupWhyChoose"));
-const WorkupProgress = dynamic(() => import("../components/agility-components/WorkupProgress"));
-const WorkupPromoTrio = dynamic(() => import("../components/agility-components/WorkupPromoTrio"));
+const WorkupProgress   = dynamic(() => import("../components/agility-components/WorkupProgress"));
+const WorkupPromoTrio  = dynamic(() => import("../components/agility-components/WorkupPromoTrio"));
+// const WorkupWhyChoose   = dynamic(() => import("../components/agility-components/WorkupWhyChoose")); // leave commented for now
+// const WorkupTestimonials = dynamic(() => import("../components/agility-components/WorkupTestimonials")); // deleted
 
-// Export module mapping
 export const moduleRegister: Record<string, any> = {
-  WorkupHeader,
-  WorkupHero,
+  WorkupHeroSlider,
+  WorkupFeatures,
   WorkupServicesGrid,
-//  WorkupTestimonials,
-//  WorkupWhyChoose,
   WorkupProgress,
-  WorkupPromoTrio
+  WorkupPromoTrio,
+  // WorkupWhyChoose,
+  // WorkupTestimonials,
 };
 
 export function getModule(name: string) {
