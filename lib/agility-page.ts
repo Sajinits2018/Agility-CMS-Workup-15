349d2b0e32b0aece78e383c25e190d9347533642
Sajinits2018 <udemysajinits24@gmail.com>
2025-10-17 11:25:37 +0530

    Update agility-page.ts

--- lib/agility-page.ts
+++ lib/agility-page.ts
@@ -1,14 +1,15 @@
 // lib/agility-page.ts
 import dynamic from "next/dynamic";
 
-// Import only components that exist
-const WorkupHeroSlider = dynamic(() => import("../components/agility-components/WorkupHeroSlider"));
-const WorkupFeatures   = dynamic(() => import("../components/agility-components/WorkupFeatures"));
+// Import only components that exist in /components/agility-components
+const WorkupHeroSlider   = dynamic(() => import("../components/agility-components/WorkupHeroSlider"));
+const WorkupFeatures     = dynamic(() => import("../components/agility-components/WorkupFeatures"));
 const WorkupServicesGrid = dynamic(() => import("../components/agility-components/WorkupServicesGrid"));
-const WorkupProgress   = dynamic(() => import("../components/agility-components/WorkupProgress"));
-const WorkupPromoTrio  = dynamic(() => import("../components/agility-components/WorkupPromoTrio"));
-// const WorkupWhyChoose   = dynamic(() => import("../components/agility-components/WorkupWhyChoose")); // leave commented for now
-// const WorkupTestimonials = dynamic(() => import("../components/agility-components/WorkupTestimonials")); // deleted
+const WorkupProgress     = dynamic(() => import("../components/agility-components/WorkupProgress"));
+const WorkupPromoTrio    = dynamic(() => import("../components/agility-components/WorkupPromoTrio"));
+// Leave these disabled until their helper imports are fixed
+// const WorkupWhyChoose    = dynamic(() => import("../components/agility-components/WorkupWhyChoose"));
+// const WorkupTestimonials = dynamic(() => import("../components/agility-components/WorkupTestimonials"));
 
 export const moduleRegister: Record<string, any> = {
   WorkupHeroSlider,
