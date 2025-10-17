e64ca0ae2fee2460e4e1ddaef9a2cb0038f205e6
Sajinits2018 <udemysajinits24@gmail.com>
2025-10-17 15:28:12 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import "@/styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Meta & fonts */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      </Head>
+
+      {/* Load Workup starter JS files */}
+      <Script
+        src="/assets/workup-starter/assets/js/jquery-3.3.1.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="/assets/workup-starter/assets/js/bootstrap.min.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="/assets/workup-starter/assets/js/theme-change.js"
+        strategy="afterInteractive"
+      />
+
+      {/* Render the active page */}
+      <Component {...pageProps} />
+    </>
+  );
 }
